b84c93e9bc5e40a391653c6890d92d60bf592518
KHAIRI <mkhairiyusoff.1985@gmail.com>
2024-01-07 00:09:57 +0800

    Create scraper

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 
-import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/scraper/utils";
-import { connectToDB } from "@/lib/scraper/mongoose";
+import { getLowestPrice, getHighestPrice, getAveragePrice, getEmailNotifType } from "@/lib/utils";
+import { connectToDB } from "@/lib/mongoose";
 import Product from "@/lib/models/product.model";
-import { scrapeAmazonProduct } from "@/lib/scraper/utils";
+import { scrapeAmazonProduct } from "@/lib/utils";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
 export const maxDuration = 300; // This function can run for a maximum of 300 seconds
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -2,9 +2,9 @@
 
 import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
-import { connectToDB } from "../scraper/mongoose";
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
-import { getAveragePrice, getHighestPrice, getLowestPrice } from "../scraper/utils";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 import { User } from "@/types";
 import { generateEmailBody, sendEmail } from "../nodemailer";
 
